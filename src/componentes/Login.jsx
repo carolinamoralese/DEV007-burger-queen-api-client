b9f894aba7d56b366b701240046abb82dbd8e171
PaosLuna <lunapp20@gmail.com>
2023-07-26 16:17:17 -0600

    Iniciando interfaz Login

--- src/componentes/Login.jsx
+++ src/componentes/Login.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 import '../estilos/login.css'
+import logo from '../Imagenes/logo.png'
 
 function Login() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     return (
-    <>
-    <>
+    <div className='container-login'>
+    <div className='container-form'>
         <h1 className='titulo'>
         Comida todo el día, todos los días
         </h1><br></br>
-        </>
         <form>
             <label htmlFor='email' className='login-app'>Correo:</label><br></br>
             <input type='email' id='email' value={email} onChange={(e) => {
@@ -21,7 +21,12 @@ function Login() {
                 setPassword(e.target.value)
             }}></input>
         </form>
-    </>
+    </div>
+    <div className='container-logo'>
+    <img src={logo} className='logo'/>
+    </div>
+    
+    </div>
     )
 }
 
