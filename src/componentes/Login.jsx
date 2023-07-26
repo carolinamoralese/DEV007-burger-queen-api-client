356ca3a639b43e01e2120ab49d5161041374df3c
PaosLuna <lunapp20@gmail.com>
2023-07-26 15:40:22 -0600

    Feat inputs login

--- src/componentes/Login.jsx
+++ src/componentes/Login.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
+import '../estilos/login.css'
 
 function Login() {
-    const [email, setEmail] = useState('hola')
-
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
     <>
+    <>
+        <h1 className='titulo'>
+        Comida todo el día, todos los días
+        </h1><br></br>
+        </>
         <form>
-            <label htmlFor='email'>Correo:</label>
+            <label htmlFor='email' className='login-app'>Correo:</label><br></br>
             <input type='email' id='email' value={email} onChange={(e) => {
                 setEmail(e.target.value)
+            }}></input><br></br>
+            <label htmlFor='password' className='login-app'>Contraseña:</label><br></br>
+            <input type='password' id='password' value={password} onChange={(e) => {
+                setPassword(e.target.value)
             }}></input>
-
         </form>
     </>
     )
