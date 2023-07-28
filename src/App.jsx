ba814cae083a3ebb3369ba0587481d61bebe682f
PaolaOtamendi <paola_otamendi23@hotmail.com>
2023-07-28 09:59:11 -0600

    feat react router dom 1 fase

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import {Routes, Route} from 'react-router-dom'
 import './App.css'
 import Login from './componentes/Login'
+import Product from './componentes/Product'
+
 
 function App() {
 
   return (
-    <>
-      <Login></Login>
-    </>
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/Menu' element={<Product />} />
+    </Routes>
+    /*<>
+    <Login />;
+    <Product />;
+    </>*/
   )
 }
 
---
+++ src/componentes/Product.jsx
@@ -0,0 +1,14 @@
+import '../estilos/menu.css'
+
+function Product() {
+
+    return (
+    <>
+        <h1 className='product'>
+        Aqui va el menu
+        </h1>
+    </>
+    )
+}
+
+export default Product;
