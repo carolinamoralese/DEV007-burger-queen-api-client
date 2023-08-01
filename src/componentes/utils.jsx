72301c9c6afb7d6bceaf6e23b2d2bcf3d326f16b
carolinamoralese <caromorales1396@gmail.com>
2023-08-01 14:36:10 -0500

    se continua haciendo la peticion para productos

--- src/componentes/utils.jsx
+++ src/componentes/utils.jsx
@@ -2,24 +2,17 @@
 
 //export function Productos() {
 
-    const requestOptions = {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json" ,
-          "authorization": "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBhby5ob3BwZXJAc3lzdGVycy54eXoiLCJpYXQiOjE2OTA5MTEyNDYsImV4cCI6MTY5MDkxNDg0Niwic3ViIjoiNCJ9.fzphPU_EiUY0L8shxVB2i9hMG3KH8WQThUc10_lfVdc"
-      },// debe ser variable el bearer cada que se ha
-        };
-        fetch("http://localhost:8080/products", requestOptions)
-        .then((response) => response.json())
-        .then((responseJson) => {
-            console.log(responseJson);
-        })
-        .catch((error) => {
-            console.error(error.mensaje);
-        });
+export const requestOptions = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    authorization:
+      "Bearer " +
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdyYWNlLmhvcHBlckBzeXN0ZXJzLnh5eiIsImlhdCI6MTY5MDkxNzIxOCwiZXhwIjoxNjkwOTIwODE4LCJzdWIiOiIyIn0.tsTnxWWTXyx5wDoWHyWE5gLMKcoJNb8QhhnPXodqkCw",
+  }, // debe ser variable el bearer cada que se ha
+};
 
 
-    
 const productos = requestOptions;
 
 // return (
@@ -38,19 +31,23 @@ const productos = requestOptions;
 //}
 
 export function Desayunos() {
-const breakfast = productos.filter(producto => producto.carta === 'desayuno');
+  const breakfast = productos.filter(
+    (producto) => producto.carta === "desayuno"
+  );
 
-return (
-    breakfast.map(producto => (
-        <div key={producto.id} className='product'>
-        <img className='imagen' src={producto.image} alt='Coffee'></img>
-        <div className='container-description'>
-        <p className='description'>
-            {producto.name}<br />{producto.price}
+  return breakfast.map((producto) => (
+    <div key={producto.id} className="product">
+      <img className="imagen" src={producto.image} alt="Coffee"></img>
+      <div className="container-description">
+        <p className="description">
+          {producto.name}
+          <br />
+          {producto.price}
         </p>
-        <button className='add'>+</button>
-        </div>
+        <button className="add">+</button>
+      </div>
     </div>
-    ))
-);
+  ));
 }
+/// productos debe venir por props
+///
