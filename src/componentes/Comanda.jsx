0b1be571d7ab3b82df470c5a3ef040c01cb12dbb
PaosLuna <lunapp20@gmail.com>
2023-08-08 10:43:04 -0600

    Suma de productos

--- src/componentes/Comanda.jsx
+++ src/componentes/Comanda.jsx
@@ -1,11 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "../estilos/comanda.css";
 
 function Comanda({ onMount, order, onDeleteItem }) {
   useEffect(() => {
     onMount();
   }, []);
-  let cantidad = 1
+  
+  //let cantidad = 1
+
+  const [cantidad, setCantidad] = useState(1); //Declaramos cantidad de forma correcta
+
+  // Calcula el total de los productos en la orden
+  const totalCuenta = order.productos.reduce((suma, producto) => (
+    suma + (cantidad * producto.price)
+  ), 0);
+
+  const aumentar = () => {
+    setCantidad(cantidad + 1);
+  };
+
+  const disminuir = () => {
+    if (cantidad > 1) {
+      setCantidad(cantidad - 1);
+    }
+  };
+
+
   return (
     <div className="container-principal">
       <div className="container-order">
@@ -26,9 +46,9 @@ function Comanda({ onMount, order, onDeleteItem }) {
             <p className="precio">{producto.price}</p>
             </div>
             <div className="botonesCantidad">
-            <button className="mas">+</button>
+            <button className="mas" onClick={aumentar}>+</button>
             <p className="suma">{cantidad}</p>
-            <button className="menos">-</button>
+            <button className="menos" onClick={disminuir}>-</button>
             <button className="eliminar" onClick={() => onDeleteItem(index)}>Eliminar</button>
             </div>
           </div>
@@ -37,7 +57,7 @@ function Comanda({ onMount, order, onDeleteItem }) {
         <div className="container-total">
           <p className="info-pedidos">TOTAL</p>
           <p className="info-total">
-            <span>$</span>
+            <span>${totalCuenta}</span>
           </p>
           <button className="boton-orden">CREAR ORDEN</button>
         </div>
