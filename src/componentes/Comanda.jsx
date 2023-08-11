d02b771ba206184a9742a36cd0c74b8e361d4007
carolinamoralese <caromorales1396@gmail.com>
2023-08-11 15:52:27 -0500

    se agrega validaciones para mostrar los alerts de orden creada y aler de error cuando la orden no fue creada con exito

--- src/componentes/Comanda.jsx
+++ src/componentes/Comanda.jsx
@@ -1,112 +1,159 @@
 import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 import "../estilos/comanda.css";
 
-
-
-function Comanda({ onMount, order, onAddProduct, onLessProduct, onDeleteItem }) {
+function Comanda({
+  onMount,
+  order,
+  onAddProduct,
+  onLessProduct,
+  onDeleteItem,
+  onResetOrder,
+}) 
+{
   useEffect(() => {
     onMount();
   }, []);
 
   // Calcula el total de los productos en la orden
-  const totalCuenta = order.productos.reduce((suma, producto) => (
-    suma + (producto.quantity * producto.price)
-  ), 0);
+  const totalCuenta = order.productos.reduce(
+    (suma, producto) => suma + producto.quantity * producto.price,
+    0
+  );
 
-function addProduct(product) {
+  function addProduct(product) {
     onAddProduct(product);
   }
 
-function lessProduct(product) {
-  onLessProduct(product);
+  function lessProduct(product) {
+    onLessProduct(product);
   }
 
+  function showAlertSucces() {
+    Swal.fire({
+      icon: "success",
+      title: "Tu orden ha sido creada con exito!",
+      showConfirmButton: false,
+      timer: 2500,
+    });
+  }
 
-  //guarda el nombre del cliente 
+  function showAlertError() {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Error con tu orden, verifica tu pedido!",
+    });
+  }
+
+  //guarda el nombre del cliente
   const [client, setClient] = useState(""); //declaramos el estado del nombre del cliente
 
   const handleClient = (event) => {
-    const nombreCliente = event.target.value
-    console.log(nombreCliente);
+    const nombreCliente = event.target.value;
     if (nombreCliente) {
-        setClient(nombreCliente);
+      setClient(nombreCliente);
     }
-};
+  };
 
-//guarda el pedido
-function PeticionPostOrders() {
-  const bearerToken = localStorage.getItem("token");
-  if (client) {
-    console.log(order, 611111111);
-    const requestOptions = {
+  //guarda el pedido
+  function PeticionPostOrders() {
+    const bearerToken = localStorage.getItem("token");
+    if (client && order.productos.length > 0) {
+      console.log(order, 611111111);
+      const requestOptions = {
         method: "POST",
         headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + bearerToken,
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + bearerToken,
         },
         body: JSON.stringify({
-        client: client,
-        products: order.productos, 
-        status: "pending",
-        dataEntry: new Date().toLocaleString(),
-        cantidad: order.quantity,
-        startTime: new Date(),
+          client: client,
+          products: order.productos,
+          status: "pending",
+          dataEntry: new Date().toLocaleString(),
+          cantidad: order.quantity,
+          startTime: new Date(),
+        }),
+      };
+      fetch("http://localhost:8080/orders", requestOptions)
+        .then((response) => response.json())
+        .then((responseJson) => {
+          showAlertSucces();
+          localStorage.removeItem('order');
+          onResetOrder();
+          setClient('')
+          
         })
-      } 
-  fetch("http://localhost:8080/orders", requestOptions)
-      .then((response) => response.json())
-      .then((responseJson) => {
-      console.log('respuesta exitosa', responseJson);
-      })
-      .catch((error) => {
-      console.error("error en la petición", error);
-      });
+        .catch((error) => {
+          console.error(error);
+        });
     } else {
-      console.log('error');
+      showAlertError();
+      console.log("error");
     }
-}
+  }
 
   return (
     <div className="container-principal">
       <div className="container-order">
         <h1 className="orden-pedido">PEDIDOS</h1>
-        <input className="cliente" placeholder="NOMBRE CLIENTE" value={client} onChange={handleClient}></input>
+        <input
+          className="cliente"
+          placeholder="NOMBRE CLIENTE"
+          value={client}
+          onChange={handleClient}
+        ></input>
         <div className="container-pedido">
           <p className="info-producto">PRODUCTO</p>
           <p className="info-precio">PRECIO</p>
           <p className="info-cantidad">CANTIDAD</p>
         </div>
         <div className="lista-productos">
-        {order.productos.map((producto, index) => (
-          <div className="container-lista" key={index}>
-            <div className="nombre">
-            <p className="listaComida">{producto.name}</p>
+          {order.productos.map((producto, index) => (
+            <div className="container-lista" key={index}>
+              <div className="nombre">
+                <p className="listaComida">{producto.name}</p>
+              </div>
+              <div className="container-precio">
+                <p className="precio">{producto.price}</p>{" "}
+                {/* usar P sólo cuando sean párrafos */}
+              </div>
+              <div className="botonesCantidad">
+                <button className="mas" onClick={() => addProduct(producto)}>
+                  +
+                </button>
+                {producto.quantity}
+                <button
+                  className="menos"
+                  onClick={() => lessProduct(producto.id)}
+                  aria-label="Restar un XXX"
+                >
+                  -
+                </button>
+                <button
+                  className="eliminar"
+                  onClick={() => onDeleteItem(index)}
+                >
+                  Eliminar
+                </button>
+              </div>
             </div>
-            <div className="container-precio">
-            
-            <p className="precio">{producto.price}</p> {/* usar P sólo cuando sean párrafos */}
-            </div>
-            <div className="botonesCantidad">
-            <button className="mas" onClick={() => addProduct(producto)}>+</button>
-            {producto.quantity}
-            <button className="menos"  onClick={() => lessProduct(producto.id)} aria-label="Restar un XXX">-</button>
-            <button className="eliminar" onClick={() => onDeleteItem(index)}>Eliminar</button>
-            </div>
-          </div>
-        ))}
+          ))}
         </div>
         <div className="container-total">
           <p className="info-pedidos">TOTAL</p>
           <p className="info-total">
             <span>${totalCuenta}</span>
           </p>
-          <button className="boton-orden" onClick={PeticionPostOrders}>CREAR ORDEN</button>
+          <button className="boton-orden" onClick={PeticionPostOrders}>
+            CREAR ORDEN
+          </button>
         </div>
       </div>
     </div>
   );
 }
-
 
 export default Comanda;
 /// este es un comentario
