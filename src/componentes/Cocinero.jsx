9597f37e623b4bad287971ad97400ea457079388
PaosLuna <lunapp20@gmail.com>
2023-08-11 16:18:54 -0600

    Fx condicionar login de acuerdo a rol

---
+++ src/componentes/Cocinero.jsx
@@ -0,0 +1,12 @@
+import "../estilos/pedidos.css";
+import Pedidos from "./Pedidos";
+
+function Cocinero() {
+  return (
+    <>
+      <Pedidos></Pedidos>;
+    </>
+  );
+}
+
+export default Cocinero;
