be75bb328e5ff31a8b8e1e08c42bc754ad86c8d3
PaolaOtamendi <paola_otamendi23@hotmail.com>
2023-08-25 11:46:11 -0600

    fix test de inventario

--- src/componentes/Inventario/Inventario.test.js
+++ src/componentes/Inventario/Inventario.test.js
@@ -5,8 +5,17 @@
 // // Actualizar datos de productos.
 import React from "react";
 import Inventario from "./Invetario";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, /*fireEvent*/ } from "@testing-library/react";
+import { BrowserRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: jest.fn(),
+    useLocation: jest.fn(() => ({
+      pathname: '',
+    })),
+  }));
 
 describe("Inventario component", () => {
     it('renders product information correctly', () => {
@@ -14,19 +23,74 @@ describe("Inventario component", () => {
         {id: 1, name:'Producto 1', price: 10},
         {id: 2, name:'Producto 2', price: 20}
     ];
-    render(<Inventario />);
-    const productContainers = screen.getAllByTestId('product-container')
+
+    const navigateMock = jest.fn();
+    require('react-router-dom').useNavigate.mockReturnValue(navigateMock);
+
+    render(
+    <BrowserRouter>
+    <Inventario />
+    </BrowserRouter>
+    );
+
+    /* const productContainers = screen.getAllByTestId('product-container')
     expect(productContainers).toHaveLength(productsMock.length);
+
     productContainers.forEach((container, index) => {
         const product = productsMock[index];
-        expect(container.querySelector('.productId')).toHaveTextContent(String(product.id));
-        expect(container.querySelector('.productName')).toHaveTextContent(product.name);
-        expect(container.querySelector('.productPrice')).toHaveTextContent(String(product.price));
+        const containerWithinModal = within(container);
+        
+        expect(containerWithinModal.getByText(String(product.id))).toBeInTheDocument();
+        expect(containerWithinModal.getByText(product.name)).toBeInTheDocument();
+        expect(containerWithinModal.getByText(String(product.price))).toBeInTheDocument(); */
+
+    const productContainers = screen.getAllByTestId('product-container');
+    productContainers.forEach((container, index) => {
+    const product = productsMock[index];
+
+  // Usa within para acceder a los elementos dentro del contenedor
+    const containerWithin = within(container);
+
+    expect(containerWithin.getByText(String(product.id))).toBeInTheDocument();
+    expect(containerWithin.getByText(product.name)).toBeInTheDocument();
+    expect(containerWithin.getByText(String(product.price))).toBeInTheDocument();
     });
 });
 });
 
 
+describe('Inventario component', () => {
+    it('renders product information correctly', async () => {
+      // Crear un mock de productos
+      const productsMock = [
+        { id: 1, name: 'Producto 1', price: 10 },
+        { id: 2, name: 'Producto 2', price: 20 },
+      ];
+  
+
+    const navigateMock = jest.fn();
+    require('react-router-dom').useNavigate.mockReturnValue(navigateMock);
+      // Mockear la función getProducts para devolver los productos mockeados
+      jest.spyOn(API, 'getProducts').mockResolvedValue(productsMock);
+  
+      // Renderizar el componente
+      render(
+        <BrowserRouter>
+        <Inventario />
+        </BrowserRouter>
+      );
+  
+      // Esperar a que los productos se carguen (esto dependerá de cómo se realiza la carga en la función useEffect)
+      await screen.findAllByTestId('product-container');
+  
+      // Verificar que los productos se renderizan correctamente en el DOM
+      productsMock.forEach((product) => {
+        expect(screen.getByText(String(product.id))).toBeInTheDocument();
+        expect(screen.getByText(product.name)).toBeInTheDocument();
+        expect(screen.getByText(String(product.price))).toBeInTheDocument();
+      });
+    });
+  });
 
 
 
