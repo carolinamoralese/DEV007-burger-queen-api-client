83a857fef91bde0ffc51cba633b1af00146b4d99
PaosLuna <lunapp20@gmail.com>
2023-08-28 09:12:40 -0600

    css final

--- src/componentes/Inventario/EditarProductos.jsx
+++ src/componentes/Inventario/EditarProductos.jsx
@@ -1,30 +1,29 @@
 import { useState } from "react";
-import "./editarProductos.css";
-
+import "./EditarProductos.css";
 
 /*---------------------------- COMPONENTE QUE MUESTRA EL FORM PARA ACTULIZAR DATOS DEL PRODUCTO -----------------------*/
 
 const EditarProductos = ({ onSaveChanges }) => {
-    const [newPrice, setNewPrice] = useState("");
-    
-    const handleSaveChanges = () => {
-        onSaveChanges(newPrice);
-    };
+  const [newPrice, setNewPrice] = useState("");
 
-    return (
-        <div className="container-editor-productos">
-            <h1 className="title-editar">EDITAR PRODUCTOS</h1>
-            <input 
-            className="nuevo-precio"
-            placeholder="Ingresa nuevo precio"
-            value={newPrice}
-            onChange={(e) => setNewPrice(e.target.value)}
-            ></input>
-            <button className="btn-aceptar-cambios" onClick={handleSaveChanges}>
-                REALIZAR CAMBIOS
-            </button>
-        </div>
-    );
+  const handleSaveChanges = () => {
+    onSaveChanges(newPrice);
+  };
+
+  return (
+    <div className="container-editor-productos">
+      <h1 className="title-editar">EDITAR PRODUCTOS</h1>
+      <input
+        className="nuevo-precio"
+        placeholder="Ingresa nuevo precio"
+        value={newPrice}
+        onChange={(e) => setNewPrice(e.target.value)}
+      ></input>
+      <button className="btn-aceptar-cambios" onClick={handleSaveChanges}>
+        REALIZAR CAMBIOS
+      </button>
+    </div>
+  );
 };
 
 export default EditarProductos;
